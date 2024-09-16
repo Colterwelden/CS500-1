35ea257b4ea8071a5c1cc2550cdf80b1c9b00dda
Colter Welden <colterwelden@gmail.com>
2024-09-15 17:26:39 -0700

    Original

---
+++ crit_think_1.py
@@ -0,0 +1,19 @@
+# CSC500 Critical Thinking Module 1
+
+num1 = float(input("What is the first number? "))
+num2 = float(input("What is the second number? "))
+addition = num1 + num2
+subtraction = num1 - num2 
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+
+num1 = float(input("What is the first number? "))
+num2 = float(input("What is the second number? "))
+multiply = num1 * num2
+if num2!= 0:
+  divide = num1 / num2
+else:
+  divide = "undefined" 
+print("multiplication:", multiply)
+print("Division:", divide)
+
